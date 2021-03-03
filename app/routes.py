a9dcbd1edbca8c4dafcfeeeb4468e91b3f71ae8f
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-03 14:33:57 -0500

    forms

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,19 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/home')
 def index():
-    return render_template("home.html")
+    return render_template("home.html", title='Home')
 
 
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Sign in", form=form)
 
 
 
