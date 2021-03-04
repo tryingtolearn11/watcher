1700c33f107c7df714cec4f3c72c204578cc95f6
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-04 15:50:36 -0500

    scratch

--- .flaskenv
+++ .flaskenv
@@ -1,3 +1,2 @@
-FLASK_APP=watcher.py
 FLASK_ENV=development
 FLASK_DEBUG=1
---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+
+
+
+app = Flask(__name__)
+
+
+
+
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return "Home Page"
+
+
+if __name__ =='__main__':
+    app.run()
