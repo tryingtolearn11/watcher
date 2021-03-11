6f2b89696c7b2a80d42396da35e37782131528f3
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-10 23:08:47 -0500

    need to connect db to client

--- app/routes.py
+++ app/routes.py
@@ -6,12 +6,41 @@ from pycoingecko import CoinGeckoAPI
 import pprint
 
 
+# TODO: TAKE THE DATA FROM DB AND CONNECT IT TO coins()
+res = []
 @scheduler.task('interval', id='do_job_1', seconds=60)
 def job1():
     with scheduler.app.app_context():
         print("INTERVAL JOB DONE")
+        # Get a request from api
+        data = cg.get_coins_markets(vs_currency='usd')
+        
+       #  res = []
+        for d in data:
+            rank = {k: d[k] for k in d.keys() and {'symbol','price_change_24h','name',
+                                               'current_price','market_cap_rank','market_cap'}}  
+            res.append(rank)
+    
+        keys = list(res[0].keys())
+        # print(keys)
 
 
+        # TODO: Store data in res[] to db | UPDATE: SOLVED FOR NOW I THINK
+        for i in range(len(res)):
+            # Find if coin already exists in db
+            new_coin = Coin.query.filter_by(name=res[i].get('name')).first() 
+            # If not then we add it to db
+            if new_coin is None:
+                new_coin = Coin(res[i].get('name'), res[i].get('symbol'),
+                                res[i].get('current_price'),
+                                res[i].get('market_cap_rank'),
+                                res[i].get('market_cap'),  res[i].get('price_change_24h'))
+                db.session.add(new_coin)
+                db.session.commit()
+            else:
+                db.session.merge(new_coin)
+    return res
+        
 
 # pycoingecko
 cg = CoinGeckoAPI()
@@ -34,15 +63,17 @@ def login():
     return render_template("login.html", title="Login", form=form)
 
 
-# FOR COMPARISON PURPOSES: btc : current price : 56095 
+# FOR COMPARISON PURPOSES: btc : current price : 55784 
 
 @app.route('/coins')
 def coins():
+    '''
     printer = pprint.PrettyPrinter()
-    data = cg.get_coins_markets(vs_currency='usd')
+    # data = cg.get_coins_markets(vs_currency='usd')
     # printer.pprint(data) 
     # Parse and sort for rank by market cap
-    res = []
+    # res = []
+    
     for d in data:
         rank = {k: d[k] for k in d.keys() and {'symbol','price_change_24h','name',
                                                'current_price','market_cap_rank','market_cap'}}  
@@ -66,11 +97,7 @@ def coins():
             db.session.commit()
         else:
             db.session.merge(new_coin)
-        
- 
-
-
-
+    '''
 
 
     return render_template("coin.html", title="Coins",res=res)
