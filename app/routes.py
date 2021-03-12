85e2decb08b266b51961d3bfbaef74ab1cda3f80
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-12 11:01:54 -0500

    login form done

--- app/routes.py
+++ app/routes.py
@@ -71,6 +71,9 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     form=LoginForm()
+    if form.validate_on_submit():
+        flash('Login Requested for user{}, remember_me{}'.format(form.username.data, form.remember_me.data))
+        return redirect('/index')
     return render_template("login.html", title="Login", form=form)
 
 
@@ -130,3 +133,41 @@ def coins():
 @app.route('/news')
 def news():
     return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
