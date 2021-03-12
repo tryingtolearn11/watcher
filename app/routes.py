b0638409b917611a0714afdf4f76975b9998c834
tryingtolearn11 <damiensingh17@gmail.com>
2021-03-12 18:13:09 -0500

    maybe sort by datetime

--- app/routes.py
+++ app/routes.py
@@ -43,6 +43,10 @@ def job1():
                 db.session.commit()     
         
 
+
+
+
+
 # pycoingecko
 cg = CoinGeckoAPI()
 
@@ -73,7 +77,6 @@ def logout():
     return redirect(url_for('index'))
     
 
-
 @app.route('/register', methods=['GET','POST'])
 def register():
     if current_user.is_authenticated:
@@ -99,39 +102,9 @@ def coins():
     # ISSUE: multiple coins all ranked at 100 at the bottom of list
     # To ensure we only keep the 100 coins in our list
     # I will remove the extra coins. 
+    
 
 
-
-
-
-
-    '''
-    printer = pprint.PrettyPrinter()
-    # data = cg.get_coins_markets(vs_currency='usd')
-    # printer.pprint(data) 
-    # Parse and sort for rank by market cap
-    # res = []
-    for d in data:
-        rank = {k: d[k] for k in d.keys() and {'symbol','price_change_24h','name',
-                                               'current_price','market_cap_rank','market_cap'}}  
-        res.append(rank)
-    keys = list(res[0].keys())
-    print(keys)
-    # TODO: Store data in res[] to db | UPDATE: SOLVED FOR NOW I THINK
-    for i in range(len(res)):
-        # Find if coin already exists in db
-        new_coin = Coin.query.filter_by(name=res[i].get('name')).first() 
-        # If not then we add it to db
-        if new_coin is None:
-            new_coin = Coin(res[i].get('name'), res[i].get('symbol'),
-                            res[i].get('current_price'),
-                            res[i].get('market_cap_rank'),
-                            res[i].get('market_cap'),  res[i].get('price_change_24h'))
-            db.session.add(new_coin)
-            db.session.commit()
-        else:
-            db.session.merge(new_coin)
-    '''
 
 
     return render_template("coin.html", title="Coins",all_coins=all_coins)
