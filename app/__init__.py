f6b5a45c97a03de19fde8ffacca06c79dcf2374e
tryingtolearn11 <damiensingh17@gmail.com>
2021-05-06 17:15:10 -0400

    rm

--- app/__init__.py
+++ app/__init__.py
@@ -39,12 +39,7 @@ scheduler.init_app(app)
 scheduler.start()
 
 with app.app_context():
-    if db.engine.url.drivername == 'sqlite':
-        migrate.init_app(app, db, render_as_batch=True)
-    else:
-        migrate.init_app(app,db)
-
-
+    db.create_all()
 
 
 
